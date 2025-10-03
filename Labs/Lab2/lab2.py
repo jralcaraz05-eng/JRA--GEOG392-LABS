8e86e11f18c7e0ee02e482f4299ffedefb3f3192
jralcaraz05-eng <jralcaraz05@tamu.edu>
2025-10-03 13:06:14 -0500

    lab2.py

--- Labs/Lab2/lab2.py
+++ Labs/Lab2/lab2.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     result1 = 1
 
     ### Add your code here
-
+    for num in lst1:
+        result1 *= num
     ###
 
     print(f"Results of Q1:")
@@ -16,8 +17,10 @@ if __name__ == "__main__":
     lst2 = [-1, 23, 483, 8573, -1384, -381569, 1652337, 718522177]
     result2 = 0
 
-    ### Add your code here
-
+    ### 
+    for num in lst2:
+        if num > 0:
+            result2 += num
     ###
 
     print(f"Results of Q2:")
@@ -27,8 +30,10 @@ if __name__ == "__main__":
     lst3 = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21]
     result3 = 0
 
-    ### Add your code here
-
+    ### 
+    for num in lst3:
+        if num % 2 == 0:
+            result3 += num
     ###
 
     print(f"Results of Q3:")
